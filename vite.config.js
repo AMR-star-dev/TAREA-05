cc18072ef556dbad16391524c717d6489d279e44
AMR-star-dev <anibalmallmarojas9Qgmail.com>
2026-05-17 19:39:56 -0500

    fix: base path github pages.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/TAREA-05/", // 👈 ESTA LÍNEA ES LA CLAVE
+
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
